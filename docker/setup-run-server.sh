a3cb575e71c05d2fdbf778acade1611e87d622e7
Elsio Sanchez <elsiosanches@gmail.com>
2024-06-18 17:52:31 -0400

    Change Router the Job

--- docker/setup-run-server.sh
+++ docker/setup-run-server.sh
@@ -7,12 +7,12 @@ sed -i "s|@TARGET_DB_NAME@|$TARGET_DB_NAME|g" /opt/pentaho/repo/Main_JOB.kjb
 sed -i "s|@TARGET_DB_USERNAME@|$TARGET_DB_USERNAME|g" /opt/pentaho/repo/Main_JOB.kjb
 sed -i "s|@TARGET_DB_PASSWORD@|$TARGET_DB_PASSWORD|g" /opt/pentaho/repo/Main_JOB.kjb
 
-sed -i "s|@EMAIL_SMTP_SERVER@|$EMAIL_SMTP_SERVER|g" /opt/pentaho/repo/Main_JOB.kjb
-sed -i "s|@EMAIL_SMTP_PORT@|$EMAIL_SMTP_PORT|g" /opt/pentaho/repo/Main_JOB.kjb
-sed -i "s|@EMAIL_SENDER_ADDRESS@|$EMAIL_SENDER_ADDRESS|g" /opt/pentaho/repo/Main_JOB.kjb
-sed -i "s|@EMAIL_PASSWORD@|$EMAIL_PASSWORD|g" /opt/pentaho/repo/Main_JOB.kjb
-sed -i "s|@EMAIL_SENDER_NAME@|$EMAIL_SENDER_NAME|g" /opt/pentaho/repo/Main_JOB.kjb
-sed -i "s|@EMAIL_DESTINATION_ADDRESS@|$EMAIL_DESTINATION_ADDRESS|g" /opt/pentaho/repo/Main_JOB.kjb
+sed -i "s|@EMAIL_SMTP_SERVER@|$EMAIL_SMTP_SERVER|g" /opt/pentaho/repo/Job/*.kjb
+sed -i "s|@EMAIL_SMTP_PORT@|$EMAIL_SMTP_PORT|g" /opt/pentaho/repo/Job/*.kjb
+sed -i "s|@EMAIL_SENDER_ADDRESS@|$EMAIL_SENDER_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
+sed -i "s|@EMAIL_PASSWORD@|$EMAIL_PASSWORD|g" /opt/pentaho/repo/Job/*.kjb
+sed -i "s|@EMAIL_SENDER_NAME@|$EMAIL_SENDER_NAME|g" /opt/pentaho/repo/Job/*.kjb
+sed -i "s|@EMAIL_DESTINATION_ADDRESS@|$EMAIL_DESTINATION_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
 
 
 sed -i "s|@HOST_URL@|$HOST_URL|g" /opt/pentaho/repo/Job/*.kjb
